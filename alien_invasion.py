d11d7c2122fcde0ee7ccae7fe4b5b35570404df1
Liguangru <1124659161@qq.com>
2022-09-05 20:17:04 +0800

    设置背景色

--- alien_invasion.py
+++ alien_invasion.py
@@ -16,6 +16,9 @@ class AlienInvasion:
         ##调用set_mode来创建显示窗口，宽1200像素，高800像素，并将显示窗口赋给属性self.screen使得类中的每个对象都可以使用它
         pygame.display.set_caption("Alian Invasion")
 
+        ## 设置背景色
+        self.bg_color = (230, 230, 230)  ##RBG值表示颜色
+
     def run_game(self):
         """开始游戏的主循环"""
         while True:  ##不断运行的循环，包含一个事件循环（玩家操作）以及屏幕更新代码
@@ -23,6 +26,10 @@ class AlienInvasion:
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     sys.exit()
+
+
+            """每次循环时都重绘屏幕"""
+            self.screen.fill(self.bg_color) ##fill用于填充屏幕，只有一个实参——颜色
 
             """让最近绘制的屏幕可见"""
             pygame.display.flip()  ##每次while循环都会绘制一个空屏幕，并擦去旧屏幕
