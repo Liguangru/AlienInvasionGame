e6c973e1180974077d8f3faca40250b454ed54f5
Liguangru <1124659161@qq.com>
2022-09-06 11:09:47 +0800

    整理注释格式

--- alien_invasion.py
+++ alien_invasion.py
@@ -5,39 +5,40 @@ import sys
 import pygame
 from settings import Settings
 from ship import Ship
+
+
 class AlienInvasion:
     """管理游戏资源和行为的类"""
 
     def __init__(self):
         """ 初始化游戏并创建游戏资源 """
-        pygame.init()  ##初始化背景设置，使得Pygame可以正常工作
+        pygame.init()  # 初始化背景设置，使得Pygame可以正常工作
         self.settings = Settings()
 
         self.screen = pygame.display.set_mode(
             (self.settings.screen_width, self.settings.screem_height)
         )
         self.screen = pygame.display.set_mode((1200, 800))
-        ##调用set_mode来创建显示窗口，宽1200像素，高800像素，并将显示窗口赋给属性self.screen使得类中的每个对象都可以使用它
+        # 调用set_mode来创建显示窗口，宽1200像素，高800像素，并将显示窗口赋给属性self.screen使得类中的每个对象都可以使用它
         pygame.display.set_caption("Alian Invasion")
 
         self.ship = Ship(self)
-        ## 设置背景色
-        self.bg_color = (230, 230, 230)  ##RBG值表示颜色
+        # 设置背景色
+        self.bg_color = (230, 230, 230)  # RBG值表示颜色
 
     def run_game(self):
         """开始游戏的主循环"""
-        while True:  ##不断运行的循环，包含一个事件循环（玩家操作）以及屏幕更新代码
+        while True:  # 不断运行的循环，包含一个事件循环（玩家操作）以及屏幕更新代码
             """监视键盘和鼠标事件"""
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     sys.exit()
 
-
             """每次循环时都重绘屏幕"""
-            self.screen.fill(self.settings.bg_color) ##fill用于填充屏幕，只有一个实参——颜色
+            self.screen.fill(self.settings.bg_color)  # fill用于填充屏幕，只有一个实参——颜色
             self.ship.blitme()
             """让最近绘制的屏幕可见"""
-            pygame.display.flip()  ##每次while循环都会绘制一个空屏幕，并擦去旧屏幕
+            pygame.display.flip()  # 每次while循环都会绘制一个空屏幕，并擦去旧屏幕
 
 
 if __name__ == '__main__':
--- ship.py
+++ ship.py
@@ -8,15 +8,15 @@ class Ship:
         """初始化飞船并设置其初始位置"""
         self.screen = ai_game.screen
         self.screen_rect = ai_game.screen.get_rect()
-        ##使用方法get_rect访问屏幕的属性rect（矩形对象）并将其赋给self.screen_rect，使得飞船可以放置在屏幕的正确位置
+        # 使用方法get_rect访问屏幕的属性rect（矩形对象）并将其赋给self.screen_rect，使得飞船可以放置在屏幕的正确位置
 
         """加载飞船图像并获取外接矩形"""
         self.image = pygame.image.load('images/ship.bmp')
         self.rect = self.image.get_rect()
 
-        ##对于每艘新飞船，都将其放在屏幕底部的中央
+        # 对于每艘新飞船，都将其放在屏幕底部的中央
         self.rect.midbottom = self.screen_rect.midbottom
 
     def blitme(self):
         """在指定位置绘制飞船"""
-        self.screen.blit(self.image, self.rect)  ##定义方法blitme，将图像绘制到self.rect指定的位置
+        self.screen.blit(self.image, self.rect)  # 定义方法blitme，将图像绘制到self.rect指定的位置
